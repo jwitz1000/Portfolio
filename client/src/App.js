402648e988afb3132d1964a5f2062989e135519d
Arman Riahi <namrataffy@gmail.com>
2020-05-18 11:11:53 -0700

    fixed issue for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <SideBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <SideBar />
+      <Home />
+    </div>
   );
 }
 
