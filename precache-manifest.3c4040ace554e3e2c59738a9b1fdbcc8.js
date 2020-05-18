3f3389eb9c0822388cb4bccfe5dffbbf9fa12397
Arman Riahi <namrataffy@gmail.com>
2020-05-18 15:51:13 -0700

    Updates

---
+++ precache-manifest.3c4040ace554e3e2c59738a9b1fdbcc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec72ad0c7f1dc0c9b7f749198813dbe",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "16e9f788ec95c834f383",
+    "url": "/Portfolio/static/js/2.b6f1e2a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.b6f1e2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e657623f60b6665dcbf7",
+    "url": "/Portfolio/static/js/main.c5ed52f1.chunk.js"
+  },
+  {
+    "revision": "4a50002865ce6bd649aa",
+    "url": "/Portfolio/static/js/runtime-main.d26897fe.js"
+  },
+  {
+    "revision": "b124a19b223ee095247c114f2e8cc6a2",
+    "url": "/Portfolio/static/media/Arman-1.b124a19b.png"
+  },
+  {
+    "revision": "9be22fc34e1dcfc6c3e49ec0a8eea018",
+    "url": "/Portfolio/static/media/pulpficiton.9be22fc3.png"
+  },
+  {
+    "revision": "96d1c1cf0cb64bda59d86c41ba80b8db",
+    "url": "/Portfolio/static/media/screddit.96d1c1cf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6a55103b8c9381828962b7816ab4309.js"
+  "/Portfolio/precache-manifest.3c4040ace554e3e2c59738a9b1fdbcc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
